411175304ad44b6baf37b5cc7ea6cc404d3429cd
Mika Westphal <westphal.mika@proton.me>
2026-01-28 21:05:06 +0100

    feat(BLE Service): Change from background to foreground service

--- app/src/main/java/de/polyfish0/adolightstick/routes/setup/RequiredPermissions.kt
+++ app/src/main/java/de/polyfish0/adolightstick/routes/setup/RequiredPermissions.kt
@@ -6,6 +6,7 @@ object RequiredPermissions {
     val permissions = listOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.BLUETOOTH_SCAN,
-        Manifest.permission.BLUETOOTH_CONNECT
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.POST_NOTIFICATIONS
     )
 }
--- app/src/main/java/de/polyfish0/adolightstick/service/ble/BLEService.kt
+++ app/src/main/java/de/polyfish0/adolightstick/service/ble/BLEService.kt
@@ -2,15 +2,21 @@ package de.polyfish0.adolightstick.service.ble
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.ServiceInfo
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import de.polyfish0.adolightstick.LightStickCommandBuilder
 import de.polyfish0.adolightstick.effects.Effect
 import de.polyfish0.adolightstick.service.ble.EffectManager
@@ -20,6 +26,8 @@ import kotlinx.coroutines.flow.StateFlow
 
 class BLEService: Service() {
     private val binder = LocalBinder()
+    private val NOTIFICATION_ID = 1337
+    private val CHANNEL_ID = "AdoLightStickServiceChannel"
     private lateinit var gattManager: GattManager
     private lateinit var scanner: ScanManager
     private lateinit var effectManager: EffectManager
@@ -98,17 +106,43 @@ class BLEService: Service() {
             effectManager.stop()
     }
 
+    fun startForegroundServiceWithNotification() {
+        val manager = getSystemService(NotificationManager::class.java)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Light Stick Connection",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            manager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Ado Light Stick")
+            .setContentText("Keeping connection alive...")
+            .setSmallIcon(android.R.drawable.stat_sys_data_bluetooth)
+            .setOngoing(true)
+            .build()
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_CONNECTED_DEVICE)
+        }else {
+            startForeground(NOTIFICATION_ID, notification)
+        }
+    }
+
     inner class LocalBinder: Binder() {
         fun getService(): BLEService = this@BLEService
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return START_STICKY
+        return START_NOT_STICKY
     }
 
     @SuppressLint("MissingPermission")
     override fun onCreate() {
         super.onCreate()
+
         gattManager = GattManager(
             applicationContext,
             onReady = { _deviceReady.value = true },
@@ -122,6 +156,8 @@ class BLEService: Service() {
             { _currentEffect.value = it },
             { r, g, b, a -> sendColorInternal(r, g, b, a)}
         )
+
+        startForegroundServiceWithNotification()
     }
 
     override fun onDestroy() {
