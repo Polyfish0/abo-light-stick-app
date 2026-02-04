b0e92ae705dabe46e12f72694df81cb0bc0eb3fe
AUGERClement <clement.auger@epitech.eu>
2026-02-04 22:31:29 +0100

    Test interface for Color mapping

--- app/src/main/java/de/polyfish0/adolightstick/routes/setup/PermissionRequestScreenRoute.kt
+++ app/src/main/java/de/polyfish0/adolightstick/routes/setup/PermissionRequestScreenRoute.kt
@@ -1,5 +1,8 @@
 package de.polyfish0.adolightstick.routes.setup
 
+import android.util.Log
+import android.widget.NumberPicker
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,8 +19,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
@@ -28,32 +34,66 @@ import de.polyfish0.adolightstick.routes.LightStickRoutes
 @Composable
 fun PermissionRequestScreen(navController: NavController) {
     val requiredPermissions = rememberMultiplePermissionsState(RequiredPermissions.permissions)
+    val songs = arrayOf("Magic", "Kira Kira", "Kick Back")
+    var selectedItem = "Placeholder"
 
     if(requiredPermissions.allPermissionsGranted) {
         navController.navigate(LightStickRoutes.LightStickSetup)
     }else {
         Box(
-            modifier = Modifier.Companion
+            modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
         ) {
             Column {
                 Row(
-                    modifier = Modifier.Companion.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Icon(
                         imageVector = Icons.Filled.LocationOn,
                         contentDescription = "",
-                        modifier = Modifier.Companion.size(64.dp)
+                        modifier = Modifier.size(64.dp)
                     )
                 }
                 Row {
                     Text(stringResource(R.string.allow_permissions))
                 }
                 Spacer(modifier = Modifier.weight(1f))
+                Row {
+                    @Suppress("COMPOSE_APPLIER_CALL_MISMATCH")
+                    AndroidView(
+                        factory = { context ->
+                            NumberPicker(context).apply {
+                                minValue = 0
+                                maxValue = songs.size - 1
+                                displayedValues = songs
+                                setOnValueChangedListener { _, _, newVal ->
+                                    selectedItem = songs[newVal]
+                                    //Log.d("DominantIntegration", selectedItem)
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            //.fillMaxWidth()
+                            .padding(horizontal = 50.dp)
+                            .size(width = 200.dp, height = 50.dp)
+                            .background(color = Color.White)
+                    )
+                    Button(
+                        onClick = {
+                            Log.d("DominantIntegration", selectedItem)
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.play_circle),
+                            contentDescription = stringResource(id = R.string.play_circle_description)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f))
                 Button(
-                    modifier = Modifier.Companion.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     onClick = {
                         requiredPermissions.launchMultiplePermissionRequest()
                     }
