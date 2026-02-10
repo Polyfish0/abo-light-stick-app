ee4a92a10b0ee224714deb7d0fc5ec986fcae00e
AUGERClement <clement.auger@epitech.eu>
2026-02-10 19:03:43 +0100

    Slider state hoisted

--- app/src/main/java/de/polyfish0/adolightstick/routes/MappingBuilder.kt
+++ app/src/main/java/de/polyfish0/adolightstick/routes/MappingBuilder.kt
@@ -1,5 +1,6 @@
 package de.polyfish0.adolightstick.routes
 
+import android.util.ArrayMap
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.text.isDigitsOnly
@@ -26,16 +28,20 @@ import java.util.Locale
 @Composable
 fun MappingBuilder() {
     var songDuration by remember { mutableStateOf("60") }
+    var sliderPosition by remember { mutableFloatStateOf(0f) } // How to pass to slider ?
+    var colorPosition by remember { mutableStateOf(Color.White) }
+    var mapping by remember { mutableStateOf(ArrayMap<Float, Color>()) }
+
     AdoLightStickTheme {
         Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-            OutlinedTextField( // Not yet by state
+            OutlinedTextField( // Not yet by state, maybe in next Material Version
                 value = songDuration,
                 onValueChange = { if (it.isDigitsOnly()) songDuration = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 label = { Text("Enter Clip Duration (in seconds)") }
             )
             ColorPicker()
-            SongSlider(songDuration.toIntOrNull())
+            SongSlider(songDuration.toIntOrNull(), sliderPosition, { sliderPosition = it })
             CurrentMapping() // Show gradient once model will be ready
             Button(
                 onClick = { Log.i("MappingBuilder", "Implement saving function") },
@@ -53,9 +59,8 @@ fun ColorPicker() {
 }
 
 @Composable
-fun SongSlider(nullableDuration : Int?) {
+fun SongSlider(nullableDuration : Int?, sliderPosition: Float, onValueChange: (Float) -> Unit) { // State Hoisting
     val duration = nullableDuration ?: 60
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
     val range = 0.0f..duration.toFloat() // Upper bound must be clip duration in s
 
     Column {
@@ -63,7 +68,7 @@ fun SongSlider(nullableDuration : Int?) {
             value = sliderPosition,
             valueRange = range,
             steps = (duration - 1) * 10 + 9,
-            onValueChange = { sliderPosition = it },
+            onValueChange = { onValueChange(it) },
         )
         Text(text = "Current position : ${String.format(Locale.getDefault(), "%.2f", sliderPosition)}")
     }
