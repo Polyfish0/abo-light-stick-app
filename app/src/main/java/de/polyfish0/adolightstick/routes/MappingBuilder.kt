7462a727e43cdabf969c5c535ac41357093d195e
AUGERClement <clement.auger@epitech.eu>
2026-02-15 18:53:16 +0100

    SliderWrapper compile (not yet wired)

--- app/src/main/java/de/polyfish0/adolightstick/routes/MappingBuilder.kt
+++ app/src/main/java/de/polyfish0/adolightstick/routes/MappingBuilder.kt
@@ -1,5 +1,6 @@
 package de.polyfish0.adolightstick.routes
 
+import android.R.attr.duration
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
@@ -10,11 +11,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,19 +32,23 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import de.polyfish0.adolightstick.models.MappingColor
 import de.polyfish0.adolightstick.routes.ui.theme.AdoLightStickTheme
+import de.polyfish0.adolightstick.utils.Either
 import java.util.Locale
 import java.util.SortedMap
+import java.util.stream.Collectors.mapping
+import kotlin.collections.set
+
 @Preview
 @Composable
 fun MappingBuilder() {
     var songDuration by remember { mutableStateOf("60") }
-    var sliderPosition by remember { mutableFloatStateOf(0f) } // Hoisted state
+    var sliderPosition by remember { mutableFloatStateOf(0f) } // Hoisted states
     var colorPosition by remember { mutableStateOf(Color.White) }
+    var selectedTab by remember { mutableIntStateOf(0) }
 
     val mapping = remember { mutableStateMapOf<Float, MappingColor>() }
 
@@ -52,7 +61,8 @@ fun MappingBuilder() {
                 label = { Text("Enter Clip Duration (in seconds)") }
             )
             ColorPicker() { colorPosition = it }
-            SongSlider(songDuration.toIntOrNull(), sliderPosition) { sliderPosition = it }
+            Tabs(selectedTab) {selectedTab = it}
+            FrameSlider(songDuration.toIntOrNull(), sliderPosition) { sliderPosition = it }
             CurrentMapping(fillHoles(songDuration.toIntOrNull(), mapping)) // Show gradient once model will be ready
 
             Button(
@@ -77,6 +87,21 @@ fun MappingBuilder() {
 }
 
 @Composable
+fun Tabs(selected: Int, onSelected: (Int) -> Unit) {
+
+    TabRow(selectedTabIndex = selected, modifier = Modifier.fillMaxWidth()) {
+        Tab(selected = selected == 0,
+            onClick = { onSelected(0) },
+            text = { Text("Interval") }
+        )
+        Tab(selected = selected == 1,
+            onClick = { onSelected(1) },
+            text = { Text("Frame") }
+        )
+    }
+}
+
+@Composable
 fun ColorPicker(onValueChange: (Color) -> Unit) {
     val colorPickerController = rememberColorPickerController()
 
@@ -90,8 +115,21 @@ fun ColorPicker(onValueChange: (Color) -> Unit) {
     )
 }
 
+// Prepare a Slider wrapper taking the selected and color as arg.
+// Wrapper should contain the slider and the add button
 @Composable
-fun SongSlider(nullableDuration : Int?, sliderPosition: Float, onValueChange: (Float) -> Unit) { // State Hoisting
+fun SliderWrapper(selected: Int, nullableDuration: Int?, color: Color, mapping: MutableMap<Float, MappingColor>) {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    var sliderRange by remember { mutableStateOf(0f..duration.toFloat()) }
+
+    when (selected) {
+        0 -> {IntervalSlider(nullableDuration, sliderRange) { sliderRange = it}; AddToMappingButton(mapping, color, Either.Right(sliderRange))}
+        1 -> {FrameSlider(nullableDuration, sliderPosition) { sliderPosition = it }; AddToMappingButton(mapping, color, Either.Left(sliderPosition))}
+    }
+}
+
+@Composable
+fun FrameSlider(nullableDuration : Int?, sliderPosition: Float, onValueChange: (Float) -> Unit) { // State Hoisting
     val duration = nullableDuration ?: 60
     val range = 0.0f..duration.toFloat() // Upper bound must be clip duration in s
 
@@ -103,6 +141,44 @@ fun SongSlider(nullableDuration : Int?, sliderPosition: Float, onValueChange: (F
             onValueChange = { onValueChange(it) }, // 'it' could be .999999 causing bugs. Slider can't have Int as value
         )
         Text(text = "Current position : ${String.format(Locale.getDefault(), "%.2f", sliderPosition)}")
+    }
+}
+
+@Composable
+fun IntervalSlider(nullableDuration: Int?, sliderRange: ClosedFloatingPointRange<Float>, onValueChange: (ClosedFloatingPointRange<Float>) -> Unit) { // State Hoisting
+    // Refactor those for transforming models here ?
+    // Replace sliderPos and onValueChange by Model editing.
+    val duration = nullableDuration ?: 60
+    val range = 0.0f..duration.toFloat() // Upper bound must be clip duration in s
+    var sliderPosition by remember { mutableStateOf(0f..duration.toFloat()) }
+
+    Column {
+        RangeSlider(
+            value = sliderPosition,
+            valueRange = range,
+            steps = (duration - 1) * 10 + 9,
+            onValueChange = { sliderPosition = it }, // 'it' could be .999999 causing bugs. Slider can't have Int as value
+        )
+        Text(text = "Current position : ${String.format(Locale.getDefault(), "%.2f", sliderPosition)}")
+    }
+}
+
+@Composable
+fun AddToMappingButton(mapping: MutableMap<Float, MappingColor>, colorPosition: Color, sliderPosition: Either<Float, ClosedFloatingPointRange<Float>>) {
+    // Replace Slider Pos by Either ?
+
+    Button(
+        onClick = {
+            when (sliderPosition) {
+                is Either.Left -> mapping[sliderPosition.value] = MappingColor(colorPosition, defaultColor = mapping[sliderPosition.value]?.defaultColor
+                ?: Color.Black) // Delete the whole MappingColor model
+                is Either.Right -> TODO() // Interval
+            }
+
+            Log.i("UserMapping", "Added $colorPosition at $sliderPosition")
+        }
+    ) {
+        Text("Add to Mapping")
     }
 }
 
@@ -138,6 +214,8 @@ fun fillHoles(nullableDuration : Int?, mapping: Map<Float, MappingColor>): Sorte
     }
 
     // Penser à la seconde passe
+    // Idée de base foireuse ? Comment je determine jusqu'ou j'écrase ?
+    // Ok fuck filling holes, fuck the model, instead use double slider. One for frame and one for interval.
     // Does it update the map ?
     return sorted
 }
---
+++ app/src/main/java/de/polyfish0/adolightstick/utils/Either.kt
@@ -0,0 +1,6 @@
+package de.polyfish0.adolightstick.utils
+
+sealed class Either<out A, out B> {
+    class Left<A>(val value: A): Either<A, Nothing>()
+    class Right<B>(val value: B): Either<Nothing, B>()
+}
